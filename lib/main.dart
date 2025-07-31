c21222200515024721155892286b1c3a5f155a13
Your Name <you@example.com>
2025-07-31 15:52:59 +0900

    6교시

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ return ChangeNotifierProvider(
   //이객체는 ChangeNotifier를 상속해야 하며 배분변화시 UI를 갱신할수 있다
   child:MaterialApp(
     title:'타이틀 이므니다',
-    theme:ThemeData(
+    theme:ThemeData(//앱의 전체적인 디자인 테마를 설정
       useMaterial3: true,
       colorScheme:ColorScheme.fromSeed(seedColor: Colors.deepOrange),
     ),
@@ -34,19 +34,52 @@ return ChangeNotifierProvider(
 
 class MyAppState extends ChangeNotifier{
   var current = WordPair.random();
+  //add this
+  void getNext(){//이매서드는..임의의 새 WordPair를 큐런트에 재할당
+  //또한 MyAppState를 보고있는 사람에게 알림을 보내는 notifyListeners()
+  //체인지 노티파이어 메서드를 호출
+    current = WordPair.random();
+    notifyListeners();
+  }
 }
+/*
+
+ */
 
 class MyHomePage extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     var appState = context.watch<MyAppState>();
+//add this
+var pair = appState.current;
+
     return Scaffold(
       body:Column(
         children: [
-Text('A random idea:'),
-Text(appState.current.asLowerCase),
+Text('A random AWESOME idea:'),
+BigCard(pair: pair),
+ElevatedButton(onPressed: (){
+  appState.getNext();
+  //print('button pressed!');
+},
+child:Text('Next'),
+),
         ],),
     );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(pair.asLowerCase);
   }
 }
 //앱을 대화식으로 진행하는것이 플루터를 알아가는 가장 좋은 방법입니다
